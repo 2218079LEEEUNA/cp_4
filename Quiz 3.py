d1e8b93086a37065640a5c65d3782dc4016c23e4
2218079LEEEUNA <144421698+2218079LEEEUNA@users.noreply.github.com>
2023-09-22 16:13:40 +0900

    Update Quiz 3.py

--- Quiz 3.py
+++ Quiz 3.py
@@ -1,11 +1,11 @@
-student_score = ['90','45','64','17','29']
+student_score = [90,45,64,17,29]
 result = []
 for i in student_score:
-    if i >='71':
+    if i >=71:
         result.append("A")
-    elif i >='41':
+    elif i >=41:
         result.append("B")
-    elif i >='11':
+    elif i >=11:
         result.append("C")
     else:
         result.append("D")
